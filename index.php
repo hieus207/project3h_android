79a9133e953d89ccc701b8e39295bc171f11cd5c
hieus207 <hieus207@gmail.com>
2021-07-11 02:57:05 +0700

    3

--- index.php
+++ index.php
@@ -181,10 +181,10 @@
     return $result;
   }
 
-  function updateUser($iduser,$username,$name,$permission,$recover){
+  function updateUser($iduser,$username,$name,$permission,$active,$recover){
     global $conn;
-    $table="user";
-    $sql="UPDATE `$table` SET `Username`= '$username' ,`Name`='$name',`Permission`='$permission',`Recover`='$recover' WHERE `Iduser`= '$iduser'";
+    $table="users";
+    $sql="UPDATE `$table` SET `Username`= '$username' ,`Name`='$name',`Permission`='$permission',`Active`='$active',`Recover`='$recover' WHERE `Iduser`= '$iduser'";
     // echo $sql;
     try{
       mysqli_query($conn,$sql);
@@ -346,7 +346,7 @@ if(isset($_GET['action'])&&$_GET['action']=='getAllUser'){
 if(isset($_POST['action'])&&$_POST['action']=='updateUser'){
   //
   $array1 = array(
-    'status' => updateUser($_POST['Iduser'],$_POST['Username'],$_POST['Name'],$_POST['Permission'],$_POST['Recover'])
+    'status' => updateUser($_POST['Iduser'],$_POST['Username'],$_POST['Name'],$_POST['Permission'],$_POST['Active'],$_POST['Recover'])
   );
   echo (json_encode($array1,JSON_UNESCAPED_UNICODE)); 
 }
